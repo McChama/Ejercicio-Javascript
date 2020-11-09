6217fa7dc03d648a3d87f355ec44317a253b27b2
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2020-11-08 18:59:46 -0600

    Actualizar Js

--- scripts/dice.js
+++ scripts/dice.js
@@ -6,6 +6,14 @@ function LanzarDados(){
     EvaluarResultado();
 }
 
-function EvaluarResultado(){
-
+function EvaluarResultado(Res){
+    if(Res == 7 || Res == 11){
+        /*Gano*/
+    }
+    if(Res == 2 || Res == 3 || Res == 12){
+        /*Perdio*/
+    }
+    if(Res == 4 || Res == 5 || Res == 6 || Res == 8 || Res == 9 || Res == 10){
+        /*Se establece punto*/
+    }
 }
