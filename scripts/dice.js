21f24d4488939b5a791d5f68dda6aa7333deb3f4
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2020-11-08 18:55:13 -0600

    Banner de mantenimiento - Estructura de scripts agregado

---
+++ scripts/dice.js
@@ -0,0 +1,11 @@
+function LanzarDados(){
+    var D1 = parseInt(Math.random() * (6 - 0) + 1);
+    var D2 = parseInt(Math.random() * (6 - 0) + 1);
+    var Res = D1 + D2;
+    
+    EvaluarResultado();
+}
+
+function EvaluarResultado(){
+
+}
